7037597522879839ed151b501a7203639f55b8ec
akp <parida.akshaya@gmail.com>
2018-02-10 00:04:46 -0500

    New Projects

---
+++ zipkin-service/src/main/java/com/akp/zipkinservice/ZipkinServiceApplication.java
@@ -0,0 +1,12 @@
+package com.akp.zipkinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServiceApplication.class, args);
+	}
+}
